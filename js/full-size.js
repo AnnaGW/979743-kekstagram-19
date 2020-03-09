9fa8f15f0522886bd29cd76e48ebcfd8dbc43e76
AnnaGW <a.bogoslovskaja@yandex.ru>
2020-03-09 19:57:42 +0500

    Просмотр в полноэкранном режиме (2)

--- js/full-size.js
+++ js/full-size.js
@@ -1,27 +1,21 @@
 'use strict';
- //var firstPhotoItem =
 
 (function () {
   var bigPicture = document.querySelector('.big-picture');
   var bigPictureCancel = document.querySelector('.big-picture__cancel');
   var body = document.querySelector('body');
   var commentsLoader = document.querySelector('.comments-loader');
-  var socialCommentsList = document.querySelector('.social__comments'); // список
-  //var socialCommentItemExist =  socialComments.querySelectorAll('.social__comment'); // коллекция элементов li
+  var socialCommentsList = document.querySelector('.social__comments');
 
-  //исходим из предположения, что есть хотябы один комментарий
-  var socialComment =  socialCommentsList.querySelector('.social__comment'); // существующий в верстке элемент li
-
+  var socialComment = socialCommentsList.querySelector('.social__comment');
   var closeFullsize = function () {
-      bigPicture.classList.add('hidden');
-      // удаляем созданные li
-      var socialCommentItemsNew =  socialCommentsList.querySelectorAll('.social__comment'); // коллекция элементов li
-      console.log('socialCommentItemsNew.length ' + socialCommentItemsNew.length);
-      for (var k = socialCommentItemsNew.length - 1; k > 0; k--) {
-        socialCommentItemsNew[k].remove(); // удаляем все, кроме первого
-      }
-      document.removeEventListener('keydown', onFullsizeEscPress);
-      body.classList.remove('modal-open');
+    bigPicture.classList.add('hidden');
+    var socialCommentItemsNew = socialCommentsList.querySelectorAll('.social__comment');
+    for (var k = socialCommentItemsNew.length - 1; k > 0; k--) {
+      socialCommentItemsNew[k].remove();
+    }
+    document.removeEventListener('keydown', onFullsizeEscPress);
+    body.classList.remove('modal-open');
   };
 
   var onFullsizeEscPress = function (evt) {
@@ -39,7 +33,7 @@
   };
 
   var fullsize = function (photoItem) {
-    //debugger;
+
     var bigPictureImg = document.querySelector('.big-picture__img img');
     bigPictureImg.src = photoItem.url;
     var likesСount = document.querySelector('.likes-count');
@@ -49,14 +43,11 @@
     commentsСount.textContent = photoItem.comments.length;
     var socialCaption = document.querySelector('.social__caption');
     socialCaption.textContent = photoItem.description;
-    console.log('photoItem.comments');
-    console.log(photoItem.comments);
-    //вставка комментариев
 
     commentsLoader.classList.remove('hidden');
 
-    var commentsPublished = 0; // кол-во опубликованных комментов
-    console.log('fullsize function is runing')
+    var commentsPublished = 0;
+
     if (photoItem.comments.length <= window.consts.COMMENTS_INSTALLMENT) {
       commentsPublished = photoItem.comments.length;
       commentsLoader.classList.add('hidden');
@@ -65,20 +56,18 @@
       commentsPublished = window.consts.COMMENTS_INSTALLMENT;
     }
     commentsPublishedCount.textContent = commentsPublished;
-    //вставка первого комментария в существ верстку
     insertDataComment(socialComment, photoItem.comments[0]);
-    //вставка следующих 4 комментариев, отображаемых сразу
     for (var i = 1; i < commentsPublished; i++) {
       var nextComment = socialComment.cloneNode(true);
       insertDataComment(nextComment, photoItem.comments[i]);
       socialCommentsList.appendChild(nextComment);
     }
-    var commentsQuantity = 0; // промежуточное значение
+    var commentsQuantity = 0;
 
     var onCommentsLoaderButton = function () {
       var commentsBalance = photoItem.comments.length;
-       commentsBalance = photoItem.comments.length - commentsPublished;// кол-во оставшихся комментариев
-      console.log('commentsBalance ' + commentsBalance);
+      commentsBalance = photoItem.comments.length - commentsPublished;
+
       if (commentsBalance <= window.consts.COMMENTS_INSTALLMENT) {
         commentsQuantity = photoItem.comments.length;
         commentsLoader.classList.add('hidden');
@@ -93,26 +82,19 @@
       }
       commentsPublished = commentsQuantity;
       commentsPublishedCount.textContent = commentsPublished;
-      //commentsLoader.classList.remove('hidden');
-
-      console.log('commentsQuantity ' + commentsQuantity);
-
     };
 
     commentsLoader.addEventListener('click', onCommentsLoaderButton);
 
     bigPicture.classList.remove('hidden');
 
-    //закрытие
     document.addEventListener('keydown', onFullsizeEscPress);
     bigPictureCancel.addEventListener('click', closeFullsize);
     body.classList.add('modal-open');
   };
 
-
-
-window.fullSize = {
-  fullsize: fullsize
-};
+  window.fullSize = {
+    fullsize: fullsize
+  };
 
 })();
